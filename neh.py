25d0410f2373ee25d560c002698c34ce7c8dd7ad
ksiezyk <Dupa1234>
2020-10-26 15:38:53 +0100

    prototype

--- neh.py
+++ neh.py
@@ -1,5 +1,6 @@
 import lib
 import functools
+import concurrent.futures
 import numpy as np
 import pandas as pd
 import copy
@@ -20,15 +21,19 @@ def neh(job_list, machine_dict, workers_dict, start_date, c_matrix_old=pd.DataFr
         job_list.items(), key=lambda job: job[0][1], reverse=True)
     queue.append(jobs_sorted[0])
     for i in range(1, len(job_list)):
+        queue_best = None
         c_min = pd.Timedelta.max
-        for j in range(i + 1):
-            queue_tmp = lib.insert(queue, j, jobs_sorted[i])
-            c_matrix = calculate_makespan(
-                queue_tmp, machine_dict, workers_dict, start_date, c_matrix_old)
-            c_tmp = get_c_max(start_date, c_matrix)
-            if c_min > c_tmp:
-                queue_best = queue_tmp
-                c_min = c_tmp
+        # Run each permutation on different thread
+        with concurrent.futures.ThreadPoolExecutor(max_workers=255) as executor:
+            for j in range(i + 1):
+                queue_tmp = lib.insert(queue, j, jobs_sorted[i])
+                thread = executor.submit(calculate_makespan,
+                                         *(queue_tmp, machine_dict, workers_dict, start_date, c_matrix_old))
+                c_matrix = thread.result()
+                c_tmp = get_c_max(start_date, c_matrix)
+                if c_min > c_tmp:
+                    queue_best = queue_tmp
+                    c_min = c_tmp
         queue = queue_best
     c_matrix = calculate_makespan(
         queue, machine_dict, workers_dict, start_date, c_matrix_old)
